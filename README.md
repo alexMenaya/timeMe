d2e4eee3fb3dcc3c62855a30488d537f5eb3342b
alexMenaya <alexmenaya@gmail.com>
2023-05-09 15:02:50 +0200

    Restart with Compose

--- README.md
+++
@@ -1,2 +0,0 @@
-# timeme
-Time Tracking App
--- app/src/main/java/com/alexmenaya/timeme/MainActivity.kt
+++ app/src/main/java/com/alexmenaya/timeme/MainActivity.kt
@@ -1,46 +1,46 @@
 package com.alexmenaya.timeme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.alexmenaya.timeme.databinding.ActivityMainBinding
-import com.alexmenaya.timeme.singletons.*
-import com.alexmenaya.timeme.ui.main.MainFragment
-import org.json.JSONObject
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alexmenaya.timeme.ui.theme.TimeMeTheme
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState : Bundle?) {
-
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+        setContent {
+            TimeMeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
         }
-
-        AppController.bind(this)
-        MessagingController.bind(this)
-        DataController.bind(this)
-
-        MessagingController.addObserver(NOTIFICATION_DEVICEINFO_UPDATED) { sender, dataobject ->
-            Log.d("MainActivity", "Updated Device information from $sender!!")
-            Log.d("MainActivity", "Received $dataobject!!")
-        }
-
-        MessagingController.addObserver(NOTIFICATION_RECEIVED) { sender, dataobject ->
-            Log.d("MainActivity", "Received notification")
-            Log.d("MainActivity", "Notification coming from $sender")
-            Log.d("MainActivity", "Received $dataobject")
-        }
-
-        // Mechanism to post changes
-        val data = JSONObject()
-        MessagingController.postMessage("TESTING", this, data)
-
     }
+}
 
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TimeMeTheme {
+        Greeting("Android")
+    }
 }
